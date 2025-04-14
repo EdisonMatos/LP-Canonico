73b926a583c2388d5f82bd98197c55a28fb58b78
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-14 11:38:57 -0300

    fix: Padronização

--- src/components/cards/FeatureImgOnBgCardButton.jsx
+++ src/components/cards/FeatureImgOnBgCardButton.jsx
@@ -26,7 +26,7 @@ export default function FeatureImgOnBgCardButton(props) {
   };
 
   return (
-    <MotionDivDownToUp className="flex flex-wrap justify-between w-full gap-[36px] tablet1:gap-[24px] tablet1:w-[47%] desktop1:w-[23%]">
+    <MotionDivDownToUp className="flex flex-wrap justify-between w-full gap-[36px] tablet1:gap-[24px] tablet1:w-[47%] desktop1:w-[30%]">
       <div
         className={`${bgPosition} w-full h-[560px] rounded-2xl flex flex-col justify-end bg-bottom bg-no-repeat bg-cover`}
         style={{ backgroundImage: `url(${bgImg})` }}
@@ -36,7 +36,7 @@ export default function FeatureImgOnBgCardButton(props) {
             <h1 className="font-mainFont font-medium text-[18px] desktop1:text-paragraph5 desktop2:text-[20px] mb-[10px]">
               {title}
             </h1>
-            <p className="font-secondFont text-paragraph3 desktop1:text-paragraph4 text-quaternary mb-[28px]">
+            <p className="font-secondFont text-paragraph3 desktop1:text-paragraph3 text-quaternary mb-[28px]">
               {description}
             </p>
             <div>
--- src/content/content.jsx
+++ src/content/content.jsx
@@ -22,9 +22,9 @@ import featuresImg2 from "../assets/imgs/features/imgFeatures2.jpg";
 import featuresImg3 from "../assets/imgs/features/imgFeatures3.jpg";
 import featuresImg4 from "../assets/imgs/features/imgFeatures4.jpg";
 
-import imgFeaturesCardModal1 from "../assets/imgs/features/imgFeaturesCardModal1.jpg";
-import imgFeaturesCardModal2 from "../assets/imgs/features/imgFeaturesCardModal2.jpg";
-import imgFeaturesCardModal3 from "../assets/imgs/features/imgFeaturesCardModal3.jpg";
+import imgFeaturesCardModal1 from "../assets/imgs/features/imgFeatures1.jpg";
+import imgFeaturesCardModal2 from "../assets/imgs/features/imgFeatures2.jpg";
+import imgFeaturesCardModal3 from "../assets/imgs/features/imgFeatures3.jpg";
 import imgFeaturesCardModal4 from "../assets/imgs/features/imgFeaturesCardModal4.jpg";
 
 import ctaWhatsappImgBg from "../assets/imgs/paralaxe/bgParalaxe2.jpg";
@@ -38,6 +38,8 @@ import imgTestimonial1 from "../assets/imgs/testimonials/deposition1.png";
 import imgTestimonial2 from "../assets/imgs/testimonials/deposition2.png";
 import imgTestimonial3 from "../assets/imgs/testimonials/deposition3.png";
 import imgTestimonial4 from "../assets/imgs/testimonials/deposition4.png";
+
+import { FaWhatsapp } from "react-icons/fa";
 
 const currentYear = new Date().getFullYear();
 
@@ -161,56 +163,28 @@ const content = {
         title: "Direito Financeiro",
         subtitle: (
           <p>
-            Atuação direta e especializada em: <br /> - Financiamentos e
-            empréstimos; <br /> - Golpes via PIX e Fraudes pelo celular; e<br />
-            - Negociação de dívidas.
+            - Financiamentos e empréstimos; <br /> - Golpes via PIX e Fraudes
+            pelo celular; <br /> - Negociação de dívidas.
           </p>
         ),
         description: (
           <div>
-            <p>
-              Atuação direta e especializada em: <br /> - Financiamentos e
-              empréstimos; <br /> - Golpes via PIX e Fraudes pelo celular; e
-              <br />- Negociação de dívidas.
-            </p>
+            <p></p>
           </div>
         ),
         imgModal: imgFeaturesCardModal1,
-        icon: (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-landmark"
-          >
-            <line x1="3" x2="21" y1="22" y2="22" />
-            <line x1="6" x2="6" y1="18" y2="11" />
-            <line x1="10" x2="10" y1="18" y2="11" />
-            <line x1="14" x2="14" y1="18" y2="11" />
-            <line x1="18" x2="18" y1="18" y2="11" />
-            <polygon points="12 2 20 7 4 7" />
-          </svg>
-        ),
+        icon: <FaWhatsapp />,
         buttonLabelModal: "Fale Conosco",
-        buttonLabel: "Saiba Mais",
+        buttonLabel: "Falar com Advogado",
       },
       card2: {
         img: featuresImg2,
         title: "Direito Digital",
         subtitle: (
           <p>
-            Recuperação de perfis: <br /> - Facebook; <br />
-            - Instagram; <br />
-            - Twitter (X); e <br />
-            - Tiktok. <br />
-            <br />
-            Buscamos indenização por danos morais, quando aplicável.
+            Recuperação de rede social: <br /> - Facebook, Instagram, Twitter
+            (X) e Tiktok; <br /> - Indenização por danos morais, quando
+            aplicável.
           </p>
         ),
         description: (
@@ -225,34 +199,18 @@ const content = {
             </p>
           </div>
         ),
-        imgModal: imgFeaturesCardModal4,
-        icon: (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-thumbs-down"
-          >
-            <path d="M17 14V2" />
-            <path d="M9 18.12 10 14H4.17a2 2 0 0 1-1.92-2.56l2.33-8A2 2 0 0 1 6.5 2H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.76a2 2 0 0 0-1.79 1.11L12 22a3.13 3.13 0 0 1-3-3.88Z" />
-          </svg>
-        ),
+        imgModal: imgFeaturesCardModal2,
+        icon: <FaWhatsapp />,
         buttonLabelModal: "Fale Conosco",
-        buttonLabel: "Saiba Mais",
+        buttonLabel: "Falar com Advogado",
       },
       card3: {
-        img: featuresImg2,
-        title: "Consultoria jurídica",
+        img: featuresImg3,
+        title: "Consultoria Jurídica",
         subtitle: (
           <p>
-            - Orientações legais personalizadas para pessoas e empresas;e <br />
-            - Consultoria preventiva para evitar problemas jurídicos antes que
+            - Orientações legais personalizadas para pessoas e empresas; <br />-
+            Consultoria preventiva para evitar problemas jurídicos antes que
             aconteçam.
           </p>
         ),
@@ -265,27 +223,10 @@ const content = {
             </p>
           </div>
         ),
-        imgModal: imgFeaturesCardModal2,
-        icon: (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-triangle-alert"
-          >
-            <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3" />
-            <path d="M12 9v4" />
-            <path d="M12 17h.01" />
-          </svg>
-        ),
+        imgModal: imgFeaturesCardModal3,
+        icon: <FaWhatsapp />,
         buttonLabelModal: "Fale Conosco",
-        buttonLabel: "Saiba Mais",
+        buttonLabel: "Falar com Advogado",
       },
       card4: {
         img: featuresImg3,
